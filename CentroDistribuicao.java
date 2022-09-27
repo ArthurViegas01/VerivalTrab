7e2698f5957f90d7fb341515fbb6924f1d609c8c
Pedro B B Fonseca <pedrofonseca447@gmail.com>
2022-09-27 21:45:19 +0000

    FEITORIA

--- CentroDistribuicao.java
+++ CentroDistribuicao.java
@@ -1,4 +1,4 @@
-/*
+
 public class CentroDistribuicao {
     public enum SITUACAO {
         NORMAL, SOBRAVISO, EMERGENCIA
@@ -12,48 +12,73 @@ public class CentroDistribuicao {
     public static final int MAX_ALCOOL = 2500;
     public static final int MAX_GASOLINA = 10000;
 
+    private int tAditivo;
+    private int tAlcool2;
+    private int tAlcool1;
+    private int tGasolina;
+
     public CentroDistribuicao (int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) {
-        
+        this.tAditivo=tAditivo;
+        this.tGasolina=tGasolina;
+        this.tAlcool1=tAlcool1;
+        this.tAlcool2=tAlcool2;
     }
 
-    public void defineSituacao(){
-        
-    }
+    // public void defineSituacao(String situacao){
+    //     SIUACAO(String situacao){this.siuacao=situacao}
+    // }
 
     public SITUACAO getSituacao(){
-
+        if(getGasolina()<=(MAX_GASOLINA/2)){
+            if(getAlcool1()+getAlcool2()<=(MAX_ALCOOL/2)){
+                if(tAditivo<=(MAX_ADITIVO/2)){
+                    return SITUACAO.EMERGENCIA;
+                }
+            }
+        }
+        // if(getGasolina()<=(MAX_GASOLINA/2)){
+        //     if(getAlcool1()+getAlcool2()<=(MAX_ALCOOL/2)){
+        //         if(tAditivo<=(MAX_ADITIVO/2)){
+        //             return SITUACAO.EMERGENCIA;
+        //         }
+        //     }
+        // }
+        return SITUACAO.NORMAL; 
     }
 
-    public int gettGasolina(){
-
+    public int getGasolina(){
+        return tGasolina;
     }
 
-    public int gettAditivo(){
-
+    public int getAditivo(){
+        return tAditivo;
     }
 
-    public int gettAlcool1(){
-
+    public int getAlcool1(){
+        return  tAlcool1;
     }
 
-    public int gettAlcool2(){
-
+    public int getAlcool2(){
+        return tAlcool2;
     }
 
     public int recebeAditivo(int qtdade) {
+        return qtdade;
         
     }
 
     public int recebeGasolina(int qtdade) {
+        return qtdade;
 
     }
 
     public int recebeAlcool(int qtdade) {
+        return qtdade;
 
     }
 
     public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
+        return null;
 
     }
 }
-*/
---
+++ IllegalArgumentException.java
@@ -0,0 +1,5 @@
+public class IllegalArgumentException extends RuntimeException{
+    public void IllegalAgumentExcepion(){
+        super("invalido");
+    }
+}
